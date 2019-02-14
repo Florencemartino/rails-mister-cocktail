e82fb5eaf93ad66b88132abfaa1d1f76efb549d0
Florence Martino <florence.martino.fm@gmail.com>
2019-02-14 17:13:24 +0100

    controller et view doses ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,11 +9,21 @@ class DosesController < ApplicationController
     @dose = Dose.new(cocktail_params)
     @dose.cocktail = @cocktail
     @dose.save
-    redirect_to :cocktail
+    redirect_to :cocktails
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
+
